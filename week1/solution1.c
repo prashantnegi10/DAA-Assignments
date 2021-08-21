a3660b0b9b72df2421ad6a13f227193ffa653651
prashantnegi10 <prashantnegi478@gmail.com>
2021-08-21 10:01:44 +0530

    update

--- week1/solution1.c
+++ week1/solution1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,x,m,j1;
+    int n,x,m,j1=0,k=1;
     scanf("%d",&n);
     for(int i=0;i<n;i++)
     {
@@ -9,17 +9,29 @@ int main()
         int a[x];
         for(int j=0;j<x;j++)
         {
-            scanf("%d",&x[j]);
+            scanf("%d",&a[j]);
         }
-        scanf("%d",&m)
+        scanf("%d",&m);
         for(int j=0;j<x;j++)
         {
-            if(x[j]!=m)
+            if(a[j]==m)
             {
                 j1++;
+                break;
+            }
+            else
+            {
+                k++;
             }
         }
-        printf("%d",j1);
+        if(j1==1)
+        {
+        printf("present");
+        }
+        else{
+            printf("not present");
+        }
+        printf("%d",k);
     }
     return 0;
 }
